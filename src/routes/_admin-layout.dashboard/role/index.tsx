a4b2fc0dc6fb4e6ad58581628e4c7da8a0375b8e
KhangGrey <legendofgrey123@gmail.com>
2024-05-01 00:36:41 +0700

    fix bug

--- src/routes/_admin-layout.dashboard/role/index.tsx
+++ src/routes/_admin-layout.dashboard/role/index.tsx
@@ -19,6 +19,7 @@ import { useNKRouter } from '@/core/routing/hooks/NKRouter';
 
 interface PageProps {}
 
+
 const Page: React.FunctionComponent<PageProps> = () => {
     const router = useNKRouter();
     const queryClient = useQueryClient();

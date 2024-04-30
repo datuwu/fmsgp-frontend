1c51f74dc9f10d941c886d53ad9d02fc9d3bb635
KhangGrey <legendofgrey123@gmail.com>
2024-05-01 00:38:34 +0700

    fix bug

--- src/routes/_admin-layout.dashboard/supplier-account-request/index.tsx
+++ src/routes/_admin-layout.dashboard/supplier-account-request/index.tsx
@@ -28,7 +28,6 @@ const Page: React.FunctionComponent<PageProps> = () => {
 
     useDocumentTitle('Supplier Account Request List');
 
-    
     return (
         <div>
             <div className="">
